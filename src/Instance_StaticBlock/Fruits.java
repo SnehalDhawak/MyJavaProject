140c501021b966f472043e9253ad3c2be7aa06e6
SnehalDhawak <dhawaksnehal72@gmail.com>
2025-12-15 16:06:04 +0530

    Adding changes

--- src/Instance_StaticBlock/Fruits.java
+++ src/Instance_StaticBlock/Fruits.java
@@ -1,4 +1,24 @@
 package Instance_StaticBlock;
 
 public class Fruits {
+    static {
+        System.out.println("Static block");
+    }
+    Fruits(){
+        System.out.println("Constructor of fruits");
+    }
+    Fruits(int a){
+        this();
+        System.out.println("Parameterized constructor");
+    }
+    {
+        System.out.println("Instance block");
+    }
+    void apple(){
+        System.out.println("Instance method");
+    }
+    public static void main(String[] args){
+        Fruits f = new Fruits(50);
+        f.apple();
+    }
 }
